c4988c1d4a6c40b372a9fdc5de834f826d192825
fupat002 <fupat002@students.zhaw.ch>
2021-11-23 19:03:27 +0100

    high roll added

--- src/ch/zhaw/catan/player/ThrowDice.java
+++ src/ch/zhaw/catan/player/ThrowDice.java
@@ -1,6 +1,5 @@
 package ch.zhaw.catan.player;
 
-import java.util.Iterator;
 import java.util.Random;
 import java.util.Set;
 
@@ -9,11 +8,58 @@ public class ThrowDice {
 
     /**
      * The player with the highest number has to start.
+     * @param participants  the set with all players/participants.
+     * @return              the player with the highest firs throw.
+     * @author fupat002
      */
-    public void highRoll(){
-
+    public Player highRoll(Set<Player> participants){
+        Player[] players = (Player[]) participants.toArray();
+        int[] diceThrows = new int[players.length];
+        for(int i = 0; i < players.length; i++){
+            diceThrows[i] = dice();
+        }
+        int highestDiceValue = getMaxDiceValue(diceThrows);
+        if(highestValueIsDouble(diceThrows, highestDiceValue)){
+            return highRoll(participants);
+        }else{
+            return players[indexOfPlayerWithHighestRoll(diceThrows, highestDiceValue)];
+        }
     }
 
+    private int indexOfPlayerWithHighestRoll(int[] diceThrows, int highestDiceValue){
+        for (int i = 0; i < diceThrows.length; i++){
+            if(diceThrows[i] == highestDiceValue){
+                return i;
+            }
+        }
+        return 0;
+    }
+
+    private boolean highestValueIsDouble(int[] diceThrows, int highestDiceValue){
+        int highestValueCounter = 0;
+        for(int diceThrow : diceThrows){
+            if(diceThrow == highestDiceValue){
+                highestValueCounter++;
+            }
+        }
+        return highestDiceValue < 1;
+    }
+
+    private int getMaxDiceValue(int[] inputArray){
+        int maxValue = inputArray[0];
+        for(int i=1;i < inputArray.length;i++){
+            if(inputArray[i] > maxValue){
+                maxValue = inputArray[i];
+            }
+        }
+        return maxValue;
+    }
+
+    /**
+     * rolls the dice and distributes the materials to the players.
+     * @param players   the set with all players.
+     * @author fupat002
+     */
     public void throwDice(Set<Player> players){
         int diceValue = dice();
         if(diceValue == 7){
