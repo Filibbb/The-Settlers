ed6fece8ed65b5fa2fcbda6da8dcc07020d10ee8
Philippe Weber <pw@phweber.ch>
2021-11-26 11:48:02 +0100

    board init with diceValues

--- src/ch/zhaw/catan/GameSetup.java
+++ src/ch/zhaw/catan/GameSetup.java
@@ -3,9 +3,14 @@ package ch.zhaw.catan;
 import ch.zhaw.catan.board.GameBoard;
 import ch.zhaw.hexboard.HexBoard;
 import ch.zhaw.hexboard.HexBoardTextView;
+import ch.zhaw.hexboard.Label;
 import org.beryx.textio.TextIO;
 import org.beryx.textio.TextIoFactory;
 import org.beryx.textio.TextTerminal;
+
+import java.awt.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class GameSetup {
     TextIO textIO = TextIoFactory.getTextIO();
@@ -27,7 +32,21 @@ public class GameSetup {
         GameBoard gameBoard = new GameBoard();
         HexBoard board = gameBoard.boardInit();
         HexBoardTextView view = new HexBoardTextView(board);
+        Map<Point, Integer> defaultDiceNumberPlacement = GameBoard.getDefaultDiceNumberPlacement();
+
+        for (Map.Entry<Point, Integer> e : defaultDiceNumberPlacement.entrySet()){
+
+            view.setLowerFieldLabel(e.getKey(), convertToLabel(e.getValue()));
+        }
+
         textTerminal.println(view.toString());
 
+   }
+
+   private Label convertToLabel(Integer fieldValue){
+       if (fieldValue < 10){
+           return new Label('0',fieldValue.toString().charAt(0));
+       }
+       else return new Label(fieldValue.toString().charAt(0), fieldValue.toString().charAt(1));
    }
 }
