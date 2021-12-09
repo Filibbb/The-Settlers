7fe88dc756e17a2c8f89e26b306b8312c9f9ebcc
Adrian Büchi <adrianbuechi@gmail.com>
2021-12-09 19:47:05 +0100

    moved todo trading method into bank handler

--- src/ch/zhaw/catan/GameBankHandler.java
+++ src/ch/zhaw/catan/GameBankHandler.java
@@ -10,4 +10,20 @@ public class GameBankHandler {
             Resource.BRICK, 19, Resource.WOOL, 19, Resource.GRAIN, 19, Resource.ORE, 19);
     private static final int FOUR_TO_ONE_TRADE_OFFER = 4;
     private static final int FOUR_TO_ONE_TRADE_WANT = 1;
+
+    /**
+     * Trades in  resource cards of the
+     * offered type for  resource cards of the wanted type.
+     * <p>
+     * The trade only works when bank and player possess the resource cards
+     * for the trade before the trade is executed.
+     *
+     * @param offer offered type
+     * @param want  wanted type
+     * @return true, if the trade was successful
+     */
+    public boolean tradeWithBankFourToOne(Resource offer, Resource want) {
+        // TODO: Implement
+        return false;
+    }
 }
