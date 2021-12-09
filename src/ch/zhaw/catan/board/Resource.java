1e804bbe8561e40e1f63fbf25a70611505ecb141
Adrian Büchi <adrianbuechi@gmail.com>
2021-12-09 22:12:47 +0100

    Added methods for resources

--- src/ch/zhaw/catan/board/Resource.java
+++ src/ch/zhaw/catan/board/Resource.java
@@ -18,6 +18,38 @@ public enum Resource {
         this.representation = representation;
     }
 
+    /**
+     * Checks if resource is a valid resource
+     *
+     * @param representation the representation to check
+     * @return if representation is a valid resource
+     * @author abuechi
+     */
+    public static boolean isValidResource(String representation) {
+        for (Resource resource : Resource.values()) {
+            if (resource.representation.equals(representation)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Gets resource for representation
+     *
+     * @param representation the representation to check
+     * @return resource if valid representation given
+     * @author abuechi
+     */
+    public static Resource getResourceByRepresentation(String representation) {
+        for (Resource resource : Resource.values()) {
+            if (resource.representation.equals(representation)) {
+                return resource;
+            }
+        }
+        return null;
+    }
+
     @Override
     public String toString() {
         return representation;
