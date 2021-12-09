94dcf9365550eca91dcb0fa69f321f83fc46ff68
Philippe Weber <pw@phweber.ch>
2021-12-09 18:25:53 +0100

    javadocs ^^

--- src/ch/zhaw/catan/commands/ShowBoardCommand.java
+++ src/ch/zhaw/catan/commands/ShowBoardCommand.java
@@ -6,17 +6,33 @@ import org.beryx.textio.TextIO;
 import org.beryx.textio.TextIoFactory;
 import org.beryx.textio.TextTerminal;
 
+/**
+ * Contains the logic for the ShowBoardCommand
+ *
+ * @author weberph5
+ */
 public class ShowBoardCommand implements Command {
 
     private final SettlersBoard settlersBoard;
     private final TextIO textIO = TextIoFactory.getTextIO();
     private final TextTerminal<?> textTerminal = textIO.getTextTerminal();
 
+    /**
+     * Creates an instance of ShowBoardCommand
+     *
+     * @param settlersBoard the current SettlersBoard
+     * @author weberph5
+     */
     public ShowBoardCommand(SettlersBoard settlersBoard) {
         this.settlersBoard = settlersBoard;
 
     }
 
+    /**
+     * Executes the ShowBoardCommand
+     *
+     * @author weberph5
+     */
     @Override
     public void execute() {
         final SettlersBoardTextView settlersBoardTextView = new SettlersBoardTextView(settlersBoard);
--- src/ch/zhaw/catan/commands/ShowHandCommand.java
+++ src/ch/zhaw/catan/commands/ShowHandCommand.java
@@ -6,16 +6,32 @@ import org.beryx.textio.TextIO;
 import org.beryx.textio.TextIoFactory;
 import org.beryx.textio.TextTerminal;
 
+/**
+ * Contains the logic for the ShowHandCommand
+ *
+ * @author weberph5
+ */
 public class ShowHandCommand implements Command {
 
     private final TurnOrderHandler turnOrderHandler;
     private final TextIO textIO = TextIoFactory.getTextIO();
     private final TextTerminal<?> textTerminal = textIO.getTextTerminal();
 
+    /**
+     * Creates an instance of ShowHandCommand
+     *
+     * @param turnOrderHandler the current turnOrderHandler
+     * @author weberph5
+     */
     public ShowHandCommand(TurnOrderHandler turnOrderHandler) {
         this.turnOrderHandler = turnOrderHandler;
     }
 
+    /**
+     * Executes the ShowHandCommand
+     *
+     * @author weberph5
+     */
     @Override
     public void execute() {
         Player currentPlayer = turnOrderHandler.getCurrentPlayer();
